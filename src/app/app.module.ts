692826b9ee47cf492532aa4e0292117eeaf06b0d
Darsi Sriguru Aakash <66508746+Aakashdarsi@users.noreply.github.com>
2023-11-03 23:25:14 +0530

    :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import {CalendarModule} from 'primeng/calendar';
+import { InputTextModule } from 'primeng/inputtext';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule,
+    InputTextModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
